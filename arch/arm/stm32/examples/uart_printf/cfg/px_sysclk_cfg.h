f21835596ee2b40c7dc8af207f3ebb195942e898
pieterconradie <pieter@piconomix.com>
2019-11-14 10:00:55 +0200

    Replaced px_sysclk_cfg.h with correct architecture version

--- arch/arm/stm32/examples/uart_printf/cfg/px_sysclk_cfg.h
+++ arch/arm/stm32/examples/uart_printf/cfg/px_sysclk_cfg.h
@@ -7,14 +7,14 @@
     |  __/   | |  | |___  | |_| | | |\  | | |_| | | |  | |  | |   /  \
     |_|     |___|  \____|  \___/  |_| \_|  \___/  |_|  |_| |___| /_/\_\
 
-    Copyright (c) 2013 Pieter Conradie <https://piconomix.com>
+    Copyright (c) 2018 Pieter Conradie <https://piconomix.com>
  
     License: MIT
     https://github.com/piconomix/piconomix-fwlib/blob/master/LICENSE.md
     
-    Title:          px_sysclk_cfg.h : System Clock using a TMRx peripheral configuration
+    Title:          px_sysclk.h : System Clock using the SysTick peripheral
     Author(s):      Pieter Conradie
-    Creation Date:  2013-01-16
+    Creation Date:  2018-03-06
 
 ============================================================================= */
 
@@ -26,16 +26,13 @@
 
 /* _____DEFINITIONS__________________________________________________________ */
 /// The number of system clock ticks per second
-#define PX_SYSCLK_CFG_TICKS_PER_SEC 100ul
+#define PX_SYSCLK_CFG_TICKS_PER_SEC 1000ul
 
-/// Specify which TMR peripheral must be used
-#define PX_SYSCLK_CFG_USE_TMRX 2
-
-/// Specify TMR Prescaler
-#define PX_SYSCLK_CFG_TMR_PRESCALER 1024
+/// Enable (1) or disable (0) support for STM32Cube HAL 1 ms SysTick timer functionality
+#define PX_SYSCLK_CFG_STMCUBE_HAL_TMR 1
 
 /// Specify periodic timeout (in sysclk ticks)
-#define PX_SYSCLK_CFG_TIMEOUT_PERIOD_TICKS     PX_SYSCLK_CFG_TICKS_PER_SEC 
+//#define PX_SYSCLK_CFG_TIMEOUT_PERIOD_TICKS     PX_SYSCLK_CFG_TICKS_PER_SEC
 
 /// Specify function to call on periodic timeout
 //#define PX_SYSCLK_ON_PERIODIC_TIMEOUT()    px_rtc_on_tick()
