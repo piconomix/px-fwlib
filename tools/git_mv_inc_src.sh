6fd32f72077813dee857af3f65f5d887e465abaa
Pieter Conradie <pieter@piconomix.com>
2020-05-01 12:36:21 +0200

    Added git_mv_inc_src.sh Shell script to split modules up into separate inc and src directories

---
+++ tools/git_mv_inc_src.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir inc
+mkdir src
+for f in *_template.h; do git mv "${f}" "inc/${f/_template.h/.h}"; done;
+for f in *.h; do git mv "${f}" "inc/${f}"; done;
+for f in *.c; do git mv "${f}" "src/${f}"; done;
