c7f586bf3c0873d95c0e9700e32fe6c90b4c7425
Pieter Conradie <pieter@piconomix.com>
2020-05-01 12:43:32 +0200

    Added switch to ignore mkdir error if dir already exists

--- tools/git_mv_inc_src.sh
+++ tools/git_mv_inc_src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir inc
-mkdir src
+mkdir -p inc
+mkdir -p src
 for f in *_template.h; do git mv "${f}" "inc/${f/_template.h/.h}"; done;
 for f in *.h; do git mv "${f}" "inc/${f}"; done;
 for f in *.c; do git mv "${f}" "src/${f}"; done;
