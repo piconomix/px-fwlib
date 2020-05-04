bdd32cfae6b6697acef0aa97b38706ef328c0e1a
pieterconradie <pieter@piconomix.com>
2020-05-04 10:43:28 +0200

    Renamed shell scripts

--- tools/git_mv_inc_src.sh
+++ tools/git_mv_inc_src.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir -p cfg
 mkdir -p inc
 mkdir -p src
-for f in *_cfg_template.h; do git mv "${f}" "cfg/${f/_template.h/.h}"; done;
 for f in *.h; do git mv "${f}" "inc/${f}"; done;
 for f in *.c; do git mv "${f}" "src/${f}"; done;
