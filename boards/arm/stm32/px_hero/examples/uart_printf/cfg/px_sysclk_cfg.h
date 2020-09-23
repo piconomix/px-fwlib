cf6eb7096f4f7a46f12a22804c754922edfa2acf
pieterconradie <pieter@piconomix.com>
2020-09-23 13:27:51 +0200

    Fixed PX_SYSCLK_ON_PERIODIC_TIMEOUT to PX_SYSCLK_CFG_ON_PERIODIC_TIMEOUT

--- boards/arm/stm32/px_hero/examples/uart_printf/cfg/px_sysclk_cfg.h
+++ boards/arm/stm32/px_hero/examples/uart_printf/cfg/px_sysclk_cfg.h
@@ -35,7 +35,7 @@
 //#define PX_SYSCLK_CFG_TIMEOUT_PERIOD_TICKS     PX_SYSCLK_CFG_TICKS_PER_SEC
 
 /// Specify function to call on periodic timeout
-//#define PX_SYSCLK_ON_PERIODIC_TIMEOUT()    px_rtc_on_tick()
+//#define PX_SYSCLK_CFG_ON_PERIODIC_TIMEOUT()    px_rtc_on_tick()
 
 /// @}
 #endif // #ifndef __PX_SYSCLK_CFG_H__
