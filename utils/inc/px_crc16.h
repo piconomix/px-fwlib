4cdcb2013ce289644e94c7757b2a0df3bb20914a
Pieter Conradie <pieter@piconomix.com>
2020-10-23 13:04:29 +0200

    Fix documentation

--- utils/inc/px_crc16.h
+++ utils/inc/px_crc16.h
@@ -8,33 +8,33 @@
     |_|     |___|  \____|  \___/  |_| \_|  \___/  |_|  |_| |___| /_/\_\
 
     Copyright (c) 2008 Pieter Conradie <https://piconomix.com>
- 
+
     License: MIT
     https://github.com/piconomix/piconomix-fwlib/blob/master/LICENSE.md
-    
+
     Title:          px_crc16.h : 16-bit CRC calculator
     Author(s):      Pieter Conradie
     Creation Date:  2008-11-06
 
 ============================================================================= */
 
-/** 
+/**
  *  @ingroup UTILS
  *  @defgroup PX_CRC16 px_crc16.h : 16-bit CRC calculator
- *  
+ *
  *  16-bit CRC (Cyclic Redundancy Check) calculator for checksums.
- *  
+ *
  *  File(s):
  *  - utils/inc/px_crc16.h
  *  - utils/inc/px_crc16_cfg_template.h
  *  - utils/src/px_crc16.c
- *  
+ *
  *  A CRC is an error-detecting code that is used for data integrity checks.
- *  
- *  @see http://en.wikipedia.org/wiki/Cyclic_redundancy_check 
- *  @see https://crccalc.com 
- *   
- *  Note that the polynomial value is bit reversed and a right shift operator is 
+ *
+ *  @see http://en.wikipedia.org/wiki/Cyclic_redundancy_check
+ *  @see https://crccalc.com
+ *
+ *  Note that the polynomial value is bit reversed and a right shift operator is
  *  used during the calculation.
  */
 /// @{
@@ -63,7 +63,7 @@ extern "C" {
 #endif
 /* _____DEFINITIONS__________________________________________________________ */
 /// The generator polynomial CRC16-CCITT (reversed representation):
-/// x^16 + x^12 + x^5 + x^0
+/// x^16 + x^12 + x^5 + 1
 #define PX_CRC16_POLYNOMIAL  0x8408
 /// Initial CRC value
 #define PX_CRC16_INIT_VAL    0xffff
@@ -75,32 +75,32 @@ extern "C" {
 /* _____GLOBAL VARIABLES_____________________________________________________ */
 
 /* _____GLOBAL FUNCTION DECLARATIONS_________________________________________ */
-/** 
+/**
  *  Initialise 16-bit CRC calculator.
- *   
- *  This function will initialise the table in RAM with constants to speed up 
- *  CRC calculation. For other options it does nothing. 
+ *
+ *  This function will initialise the table in RAM with constants to speed up
+ *  CRC calculation. For other options it does nothing.
  */
 void px_crc16_init(void);
 
-/** 
+/**
  *  Calculate the 16-bit CRC over one byte.
- *   
+ *
  *  @param crc          The initial CRC to start the calculation with
  *  @param data         The value to calculate the CRC over
- *   
+ *
  *  @return uint16_t    The resultant CRC
  */
 uint16_t px_crc16_update_u8(uint16_t crc, uint8_t data);
 
-/** 
+/**
  *  Calculate the 16-bit CRC over a number of bytes.
- *   
+ *
  *  @param crc          The initial CRC to start the calculation with
  *  @param data         Pointer to the data to calculate the CRC over
  *  @param nr_of_bytes  The amount of bytes to calculate the CRC over
- *   
- *  @return px_crc_t    The resultant CRC over the group of bytes
+ *
+ *  @return uint16_t    The resultant CRC over the group of bytes
  */
 uint16_t px_crc16_update_data(uint16_t crc, const void * data, size_t nr_of_bytes);
 
--- utils/inc/px_crc32.h
+++ utils/inc/px_crc32.h
@@ -8,34 +8,34 @@
     |_|     |___|  \____|  \___/  |_| \_|  \___/  |_|  |_| |___| /_/\_\
 
     Copyright (c) 2019 Pieter Conradie <https://piconomix.com>
- 
+
     License: MIT
     https://github.com/piconomix/piconomix-fwlib/blob/master/LICENSE.md
-    
+
     Title:          px_crc32.h : 32-bit CRC calculator
     Author(s):      Pieter Conradie
     Creation Date:  2019-08-06
 
 ============================================================================= */
 
-/** 
+/**
  *  @ingroup UTILS
  *  @defgroup PX_CRC32 px_crc32.h : 32-bit CRC calculator
- *  
+ *
  *  32-bit CRC (Cyclic Redundancy Check) calculator for checksums.
- *  
+ *
  *  File(s):
- *  - utils/inc/px_crc32.h 
- *  - utils/inc/px_crc32_cfg_template.h 
+ *  - utils/inc/px_crc32.h
+ *  - utils/inc/px_crc32_cfg_template.h
  *  - utils/src/px_crc32.c
- *  
+ *
  *  A CRC is an error-detecting code that is used for data integrity checks.
- *  
- *  @see http://en.wikipedia.org/wiki/Cyclic_redundancy_check 
- *  @see https://crccalc.com 
- *   
+ *
+ *  @see http://en.wikipedia.org/wiki/Cyclic_redundancy_check
+ *  @see https://crccalc.com
+ *
  *  Note that the polynomial value is bit reversed and a right shift operator is
- *  used during the calculation. 
+ *  used during the calculation.
  */
 /// @{
 
@@ -63,8 +63,8 @@ extern "C" {
 #endif
 /* _____DEFINITIONS__________________________________________________________ */
 /// The generator polynomial CRC32 (reversed representation):
-///   x^32 + x^26 + x^23 + x^22 + x^16 
-/// + x^12 + x^11 + x^10 + x^8  + x^7 + x^5 + x^4 + x^2 + x^1 + x^0
+///   x^32 + x^26 + x^23 + x^22 + x^16
+/// + x^12 + x^11 + x^10 + x^8  + x^7 + x^5 + x^4 + x^2 + x + 1
 #define PX_CRC32_POLYNOMIAL  0xedb88320
 /// Initial CRC value
 #define PX_CRC32_INIT_VAL    0xffffffff
@@ -74,32 +74,32 @@ extern "C" {
 /* _____GLOBAL VARIABLES_____________________________________________________ */
 
 /* _____GLOBAL FUNCTION DECLARATIONS_________________________________________ */
-/** 
+/**
  *  Initialise 32-bit CRC calculator.
- *   
- *  This function will initialise the table in RAM with constants to speed up 
- *  CRC calculation. For other options it does nothing. 
+ *
+ *  This function will initialise the table in RAM with constants to speed up
+ *  CRC calculation. For other options it does nothing.
  */
 void px_crc32_init(void);
 
-/** 
+/**
  *  Calculate the 32-bit CRC over one byte.
- *   
+ *
  *  @param crc          The initial CRC to start the calculation with
  *  @param data         The value to calculate the CRC over
- *   
+ *
  *  @return uint16_t    The resultant CRC
  */
 uint32_t px_crc32_update_u8(uint32_t crc, uint8_t data);
 
-/** 
+/**
  *  Calculate the 32-bit CRC over a number of bytes.
- *   
+ *
  *  @param crc          The initial CRC to start the calculation with
  *  @param data         Pointer to the data to calculate the CRC over
  *  @param nr_of_bytes  The amount of bytes to calculate the CRC over
- *   
- *  @return px_crc_t    The resultant CRC over the group of bytes
+ *
+ *  @return uint16_t    The resultant CRC over the group of bytes
  */
 uint32_t px_crc32_update_data(uint32_t crc, const void * data, size_t nr_of_bytes);
 
