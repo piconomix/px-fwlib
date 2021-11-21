1a2cb362472be99cfebe668869ae06c6bf6cdb45
pieterconradie <pieter@piconomix.com>
2021-11-21 17:53:42 +0200

    Shorten "index" to "idx"

--- utils/src/px_dbg_ft.c
+++ utils/src/px_dbg_ft.c
@@ -28,7 +28,7 @@
 
 /* _____GLOBAL VARIABLES_____________________________________________________ */
 /// Index of next empty position; Placed in .noinit section so that it is not set to zero by C initialization code before it can be inspected
-PX_ATTR_SECTION(".noinit") uint8_t px_dbg_ft_buf_index;
+PX_ATTR_SECTION(".noinit") uint8_t px_dbg_ft_buf_idx;
 
 /// Buffer for log output; Placed in .noinit section so that it is not set to zero by C initialization code before it can be inspected
 PX_ATTR_SECTION(".noinit") uint32_t px_dbg_ft_buf[PX_DBG_FT_CFG_BUF_SIZE];
@@ -57,7 +57,7 @@ const char * px_dbg_ft_name_val_to_str(px_dbg_ft_name_t name)
 /* _____GLOBAL FUNCTIONS_____________________________________________________ */
 void _px_dbg_ft_init(void)
 {
-    px_dbg_ft_buf_index = 0;
+    px_dbg_ft_buf_idx = 0;
 
     for(int i = 0; i < PX_DBG_FT_CFG_BUF_SIZE; i++)
     {
@@ -69,11 +69,11 @@ void _px_dbg_ft_log(const px_dbg_ft_name_t name, uint16_t line)
 {
     uint32_t ft = (((uint32_t)name) << 16) | ((uint32_t)line);
 
-    px_dbg_ft_buf[px_dbg_ft_buf_index] = ft;
+    px_dbg_ft_buf[px_dbg_ft_buf_idx] = ft;
 
-    if(++px_dbg_ft_buf_index == PX_DBG_FT_CFG_BUF_SIZE)
+    if(++px_dbg_ft_buf_idx == PX_DBG_FT_CFG_BUF_SIZE)
     {
-        px_dbg_ft_buf_index = 0;
+        px_dbg_ft_buf_idx = 0;
     }
 }
 
@@ -81,39 +81,39 @@ void _px_dbg_ft_log_param(const px_dbg_ft_name_t name, uint16_t line, uint8_t pa
 {
     uint32_t ft = (((uint32_t)param) << 24) | (((uint32_t)name) << 16) | ((uint32_t)line);
 
-    px_dbg_ft_buf[px_dbg_ft_buf_index] = ft;
+    px_dbg_ft_buf[px_dbg_ft_buf_idx] = ft;
 
-    if(++px_dbg_ft_buf_index == PX_DBG_FT_CFG_BUF_SIZE)
+    if(++px_dbg_ft_buf_idx == PX_DBG_FT_CFG_BUF_SIZE)
     {
-        px_dbg_ft_buf_index = 0;
+        px_dbg_ft_buf_idx = 0;
     }
 }
 
 void px_dbg_ft_report(void)
 {
-    uint8_t  index = px_dbg_ft_buf_index;
+    uint8_t  idx = px_dbg_ft_buf_idx;
     uint8_t  name, param;
     uint16_t line;
 
     do
     {
-        if(index != 0)
+        if(idx != 0)
         {
-            index--;
+            idx--;
         }
         else
         {
-            index = PX_DBG_FT_CFG_BUF_SIZE - 1;
+            idx = PX_DBG_FT_CFG_BUF_SIZE - 1;
         }
-        param = (px_dbg_ft_buf[index] >> 24) & 0xff;
-        name  = (px_dbg_ft_buf[index] >> 16) & 0xff;
-        line  = (px_dbg_ft_buf[index] >>  0) & 0xffff;
+        param = (px_dbg_ft_buf[idx] >> 24) & 0xff;
+        name  = (px_dbg_ft_buf[idx] >> 16) & 0xff;
+        line  = (px_dbg_ft_buf[idx] >>  0) & 0xffff;
 #ifdef PX_LOG_CFG_NAMES
         printf("%u %s # %u (%u)\n", name, px_dbg_ft_name_val_to_str(name), line, param);
 #else
         printf("%u # %u (%u)\n", name, line, param);
 #endif
     }
-    while(index != px_dbg_ft_buf_index);
+    while(idx != px_dbg_ft_buf_idx);
 }
 
