095ac3b359d61e5dcd075747e95dd07dc2b928ef
pieterconradie <pieter@piconomix.com>
2021-11-25 09:07:14 +0200

    Update "5.1 The PX C coding style"

--- doc/examples/doxy_ex_2.c
+++ doc/examples/doxy_ex_2.c
@@ -1,9 +1,9 @@
-/** 
+/**
  *  See if a timer has expired.
- *  
- *  @param[in,out]  systmr   Pointer to a timer object
- *  
- *  @retval true    timer expired
- *  @retval false   timer not expired or timer stopped
+ *
+ *  @param[in,out]  systmr  Pointer to a timer object
+ *
+ *  @retval true            timer expired
+ *  @retval false           timer not expired or timer stopped
  */
-extern bool px_systmr_has_expired(px_systmr_t * systmr);
+bool px_systmr_has_expired(px_systmr_t * systmr);
