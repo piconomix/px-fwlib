8d30e616ddacd4526353f293c908b28f162100f3
pieterconradie <pieter@piconomix.com>
2021-12-01 08:36:44 +0200

    Reduce number of lines of example

--- data/test/px_log_fs_test.c
+++ data/test/px_log_fs_test.c
@@ -23,9 +23,9 @@ int main(void)
     px_board_init();
     px_spi_init();
     px_spi_open2(&px_at25s_spi_handle, PX_SPI_NR_1, PX_BOARD_SPI_CS_AT25S,
-                 PX_AT25S_MAX_SPI_CLOCK_HZ, PX_AT25S_SPI_MODE,
-                 PX_AT25S_SPI_DATA_ORDER, 0x00);
+                 PX_AT25S_MAX_SPI_CLOCK_HZ, PX_AT25S_SPI_MODE, PX_AT25S_SPI_DATA_ORDER, 0x00);
     px_at25s_init(&px_at25s_spi_handle);
+
     // Initialise file system
     px_log_fs_init(&px_log_fs_handle, 0, PX_AT25S_PAGES - 1);
 
@@ -34,24 +34,18 @@ int main(void)
     for(i = 0; i < 8; i++)
     {
         // Write record
-        px_log_fs_wr(&px_log_fs_handle,
-                     &px_log_fs_data,
-                     sizeof(px_log_fs_data));
+        px_log_fs_wr(&px_log_fs_handle, &px_log_fs_data, sizeof(px_log_fs_data));
         // Increment temperature by 1.3 deg
         px_log_fs_data.temperature += 13;
     }
 
     // Read first record
-    px_log_fs_err = px_log_fs_rd_first(&px_log_fs_handle,
-                                       &px_log_fs_data,
-                                       sizeof(px_log_fs_data));
+    px_log_fs_err = px_log_fs_rd_first(&px_log_fs_handle, &px_log_fs_data, sizeof(px_log_fs_data));
     while(px_log_fs_err == PX_LOG_FS_ERR_NONE)
     {
         // Display record data
         printf("%d\n", px_log_fs_data.temp);
         // Read next record
-        px_log_fs_err = px_log_fs_rd_next(&px_log_fs_handle,
-                                          &px_log_fs_data,
-                                          sizeof(px_log_fs_data));
+        px_log_fs_err = px_log_fs_rd_next(&px_log_fs_handle, &px_log_fs_data, sizeof(px_log_fs_data));
     }
 }
