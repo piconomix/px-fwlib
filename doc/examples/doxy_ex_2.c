b55554987aa02ce221efb4987fdcadf118648cf0
pieterconradie <pieter@piconomix.com>
2021-12-08 08:05:56 +0200

    Update "6. Indentation, Brace style and Spacing" and "9. Debug instrumentation"

--- doc/examples/doxy_ex_2.c
+++ doc/examples/doxy_ex_2.c
@@ -1,9 +1,9 @@
 /**
  *  See if a timer has expired.
  *
- *  @param[in,out]  systmr  Pointer to a timer object
+ *  @param systmr       Pointer to a timer object
  *
- *  @retval true            timer expired
- *  @retval false           timer not expired or timer stopped
+ *  @retval true        timer expired
+ *  @retval false       timer not expired or timer stopped
  */
 bool px_systmr_has_expired(px_systmr_t * systmr);
