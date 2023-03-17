9580945e74d82144cd568cdd6183e0cf4841d4fe
pieterconradie <pieter@piconomix.com>
2023-03-17 12:15:10 +0200

    Inlude STM32Cube LL drivers for STM32WL series

--- arch/arm/stm32/inc/px_stm32cube.h
+++ arch/arm/stm32/inc/px_stm32cube.h
@@ -12,7 +12,7 @@
     License: MIT
     https://github.com/piconomix/px-fwlib/blob/master/LICENSE.md
  
-    Title:          px_stm32cube.h : Include file for STM32Cube library
+    Title:          px_stm32cube.h : Collective include file for STM32Cube LL drivers
     Author(s):      Pieter Conradie
     Creation Date:  2018-04-11
 
@@ -20,9 +20,9 @@
 
 /** 
  *  @ingroup STM32
- *  @defgroup STM32_STM32CUBE px_stm32cube.h : Include file for STM32Cube library
+ *  @defgroup STM32_STM32CUBE px_stm32cube.h : Collective include file for STM32Cube LL drivers
  *  
- *  This file includes all the required files from the STM32Cube library
+ *  This file includes all the required files from the STM32Cube Low Level drivers
  *  
  *  File(s):
  *  - arm/stm32/inc/px_stm32cube.h
@@ -108,6 +108,36 @@
     #include "stm32g0xx_ll_usart.h"
     #include "stm32g0xx_ll_utils.h"
     #include "stm32g0xx_ll_wwdg.h"
+
+#elif STM32WL
+
+    #include "stm32wlxx_ll_adc.h"
+    #include "stm32wlxx_ll_bus.h"
+    #include "stm32wlxx_ll_comp.h"
+    #include "stm32wlxx_ll_cortex.h"
+    #include "stm32wlxx_ll_crc.h"
+    #include "stm32wlxx_ll_dac.h"
+    #include "stm32wlxx_ll_dma.h"
+    #include "stm32wlxx_ll_dmamux.h"
+    #include "stm32wlxx_ll_exti.h"
+    #include "stm32wlxx_ll_gpio.h"
+    #include "stm32wlxx_ll_hsem.h"
+    #include "stm32wlxx_ll_i2c.h"
+    #include "stm32wlxx_ll_ipcc.h"
+    #include "stm32wlxx_ll_iwdg.h"
+    #include "stm32wlxx_ll_lptim.h"
+    #include "stm32wlxx_ll_lpuart.h"
+    #include "stm32wlxx_ll_pka.h"
+    #include "stm32wlxx_ll_pwr.h"
+    #include "stm32wlxx_ll_rcc.h"
+    #include "stm32wlxx_ll_rng.h"
+    #include "stm32wlxx_ll_rtc.h"
+    #include "stm32wlxx_ll_spi.h"
+    #include "stm32wlxx_ll_system.h"
+    #include "stm32wlxx_ll_tim.h"
+    #include "stm32wlxx_ll_usart.h"
+    #include "stm32wlxx_ll_utils.h"
+    #include "stm32wlxx_ll_wwdg.h"
 
 #else
     #error "Unsupported STM32 series"
