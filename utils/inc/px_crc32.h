461f7386574222f92c0120d6b7dc2ae118236427
pieterconradie <pieter@piconomix.com>
2023-07-25 10:01:45 +0200

    Add PX_CRC32_FINAL_VAL #define (when CRC-32 is calculated over data + CRC then the final value is 0x00000000 if the the initial CRC value is 0xffffffff)

--- utils/inc/px_crc32.h
+++ utils/inc/px_crc32.h
@@ -63,12 +63,14 @@
 extern "C" {
 #endif
 /* _____DEFINITIONS__________________________________________________________ */
-/// The generator polynomial CRC32 (reversed representation):
+/// The generator polynomial CRC-32 (reversed representation):
 ///   x^32 + x^26 + x^23 + x^22 + x^16
 /// + x^12 + x^11 + x^10 + x^8  + x^7 + x^5 + x^4 + x^2 + x + 1
 #define PX_CRC32_POLYNOMIAL  0xedb88320
 /// Initial CRC value
 #define PX_CRC32_INIT_VAL    0xffffffff
+/// Final CRC value calculated over data + CRC (very cool!)
+#define PX_CRC32_FINAL_VAL   0x00000000
 
 /* _____TYPE DEFINITIONS_____________________________________________________ */
 
