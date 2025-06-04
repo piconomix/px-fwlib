31e78e2213bc2eb946a59b52cff47077fcf5c676
pieterconradie <pieter@piconomix.com>
2025-06-04 12:33:26 +0200

    Update px_stm32cube.h to add support for STM32L4. Add more includes for STM32C0

--- arch/arm/stm32/inc/px_stm32cube.h
+++ arch/arm/stm32/inc/px_stm32cube.h
@@ -32,10 +32,12 @@
 
 /* _____PROJECT INCLUDES_____________________________________________________ */
 #if defined(STM32C0)
+
     #include "stm32c0xx_ll_adc.h"
     #include "stm32c0xx_ll_bus.h"
     #include "stm32c0xx_ll_cortex.h"
     #include "stm32c0xx_ll_crc.h"
+    #include "stm32c0xx_ll_crs.h"
     #include "stm32c0xx_ll_dma.h"
     #include "stm32c0xx_ll_dmamux.h"
     #include "stm32c0xx_ll_exti.h"
@@ -49,6 +51,7 @@
     #include "stm32c0xx_ll_system.h"
     #include "stm32c0xx_ll_tim.h"
     #include "stm32c0xx_ll_usart.h"
+    #include "stm32c0xx_ll_usb.h"
     #include "stm32c0xx_ll_utils.h"
     #include "stm32c0xx_ll_wwdg.h"
 
@@ -101,6 +104,41 @@
     #include "stm32l1xx_ll_usart.h"
     #include "stm32l1xx_ll_utils.h"
     #include "stm32l1xx_ll_wwdg.h"
+
+#elif defined(STM32L4)
+
+    #include "stm32l4xx_ll_adc.h"
+    #include "stm32l4xx_ll_bus.h"
+    #include "stm32l4xx_ll_comp.h"
+    #include "stm32l4xx_ll_cortex.h"
+    #include "stm32l4xx_ll_crc.h"
+    #include "stm32l4xx_ll_crs.h"
+    #include "stm32l4xx_ll_dac.h"
+    #include "stm32l4xx_ll_dma.h"
+    #include "stm32l4xx_ll_dma2d.h"
+    #include "stm32l4xx_ll_dmamux.h"
+    #include "stm32l4xx_ll_exti.h"
+    #include "stm32l4xx_ll_fmc.h"
+    #include "stm32l4xx_ll_gpio.h"
+    #include "stm32l4xx_ll_i2c.h"
+    #include "stm32l4xx_ll_iwdg.h"
+    #include "stm32l4xx_ll_lptim.h"
+    #include "stm32l4xx_ll_lpuart.h"
+    #include "stm32l4xx_ll_opamp.h"
+    #include "stm32l4xx_ll_pka.h"
+    #include "stm32l4xx_ll_pwr.h"
+    #include "stm32l4xx_ll_rcc.h"
+    #include "stm32l4xx_ll_rng.h"
+    #include "stm32l4xx_ll_rtc.h"
+    #include "stm32l4xx_ll_sdmmc.h"
+    #include "stm32l4xx_ll_spi.h"
+    #include "stm32l4xx_ll_swpmi.h"
+    #include "stm32l4xx_ll_system.h"
+    #include "stm32l4xx_ll_tim.h"
+    #include "stm32l4xx_ll_usart.h"
+    #include "stm32l4xx_ll_usb.h"
+    #include "stm32l4xx_ll_utils.h"
+    #include "stm32l4xx_ll_wwdg.h"
 
 #elif defined(STM32G0)
 
