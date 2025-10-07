6e953aec55b5e363887da7a4c5c0627dac6ebb12
pieterconradie <pieter@piconomix.com>
2025-10-07 15:09:30 +0200

    Improve Liteon LTR-329ALS ambient light sensor driver

--- devices/sensor/inc/px_ltr329als.h
+++ devices/sensor/inc/px_ltr329als.h
@@ -7,27 +7,27 @@
     |  __/   | |  | |___  | |_| | | |\  | | |_| | | |  | |  | |   /  \
     |_|     |___|  \____|  \___/  |_| \_|  \___/  |_|  |_| |___| /_/\_\
 
-    Copyright (c) 2020 Pieter Conradie <https://piconomix.com>
-
+    Copyright (c) 2025 Pieter Conradie <https://piconomix.com>
+ 
     License: MIT
     https://github.com/piconomix/px-fwlib/blob/master/LICENSE.md
-
-    Title:          px_ltr329als.h : Lite-On LTR329ALS light sensor sensor driver
+ 
+    Title:          px_ltr329als.h : Liteon LTR-329ALS ambient light sensor driver
     Author(s):      Pieter Conradie
-    Creation Date:  2020-10-01
+    Creation Date:  2025-07-07
 
 ============================================================================= */
 
-/**
+/** 
  *  @ingroup DEVICES_SENSOR
- *  @defgroup PX_LTR329ALS px_ltr329als.h : Lite-On LTR329ALS light sensor sensor driver
- *
+ *  @defgroup PX_LTR329ALS px_ltr329als.h : LITEON LTR-329ALS ambient light sensor driver
+ *  
  *  File(s):
  *  - devices/sensor/inc/px_ltr329als.h
  *  - devices/sensor/src/px_ltr329als.c
- *
- *  Reference:
- *  - [Lite-On LTR329ALS datasheet](https://optoelectronics.liteon.com/upload/download/DS86-2014-0006/LTR-329ALS-01_DS_V1.5.PDF)
+ *  
+ *  Reference: 
+ *  - [LITEON LTR-329ALS datasheet](https://optoelectronics.liteon.com/upload/download/DS86-2014-0006/LTR-329ALS-01_DS_V1.8.PDF)
  *
  *  @{
  */
@@ -39,14 +39,23 @@
 #include "px_i2c.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 /* _____DEFINITIONS__________________________________________________________ */
 /// LTR329ALS I2C Slave address
 #define PX_LTR329ALS_I2C_SLA_ADR        0x29
 
-/// LTR329ALS Manufacturer ID value
-#define PX_LTR329ALS_MANUFACTURER_ID    0x05
+/// Gain
+typedef enum
+{
+    PX_LTR329ALS_GAIN_1X            = 0,
+    PX_LTR329ALS_GAIN_2X            = 1,
+    PX_LTR329ALS_GAIN_4X            = 2,
+    PX_LTR329ALS_GAIN_8X            = 3,
+    PX_LTR329ALS_GAIN_48X           = 6,
+    PX_LTR329ALS_GAIN_96X           = 7,
+} px_ltr329als_gain_t;
 
 /// Integration time
 typedef enum
@@ -72,35 +81,72 @@ typedef enum
     PX_LTR329ALS_MEAS_RATE_2000MS   = 5,
 } px_ltr329als_meas_rate_t;
 
-/// Gain
-typedef enum
-{
-    PX_LTR329ALS_GAIN_1X            = 0,
-    PX_LTR329ALS_GAIN_2X            = 1,
-    PX_LTR329ALS_GAIN_4X            = 2,
-    PX_LTR329ALS_GAIN_8X            = 3,
-    PX_LTR329ALS_GAIN_48X           = 6,
-    PX_LTR329ALS_GAIN_96X           = 7,
-} px_ltr329als_gain_t;
-
 /* _____TYPE DEFINITIONS_____________________________________________________ */
+typedef struct
+{
+    uint16_t ch1;           ///< IR light
+    uint16_t ch0;           ///< Ambient light
+} px_ltr329als_meas_t;
 
 /* _____GLOBAL VARIABLES_____________________________________________________ */
 
 /* _____GLOBAL FUNCTION DECLARATIONS_________________________________________ */
-bool px_ltr329als_init                   (px_i2c_handle_t *        handle);
-bool px_ltr329als_rd_id                  (uint8_t *                id);
-void px_ltr329als_cfg                    (px_ltr329als_int_time_t  int_time,
-                                          px_ltr329als_meas_rate_t meas_rate);
-void px_ltr329als_standby_mode           (void);
-void px_ltr329als_active_mode            (px_ltr329als_gain_t      gain);
-bool px_ltr329als_meas_ready             (void);
-void px_ltr329als_meas_rd                (uint16_t *               ch0,
-                                          uint16_t *               ch1);
-uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t      gain,
-                                          px_ltr329als_int_time_t  int_time,
-                                          uint16_t                 ch0,
-                                          uint16_t                 ch1);
+/**
+ *  Initialise driver.
+ *
+ *  @param handle    I2C handle of device
+ *  @param gain      Gain
+ *  @param int_time  Integration time
+ *  @param meas_rate Measurement rate
+ *
+ *  @retval true    Device detected on I2C and Manufacturer and ID value is correct
+ *  @retval false   Device not detected on I2C bus
+ */
+bool px_ltr329als_init(px_i2c_handle_t *        handle,
+                       px_ltr329als_gain_t      gain,
+                       px_ltr329als_int_time_t  int_time,
+                       px_ltr329als_meas_rate_t meas_rate);
+
+/**
+ *  Put sensor into standby mode.
+ */
+bool px_ltr329als_standby_mode(void);
+
+/**
+ *  Put sensor into active mode.
+ *
+ *  @param gain
+ */
+bool px_ltr329als_active_mode(px_ltr329als_gain_t gain);
+
+/**
+ *  Check if new data is available to be read.
+ *
+ *  @return true    New data is available
+ *  @return false   OLD data (data already read)
+ */
+bool px_ltr329als_meas_ready(void);
+
+/**
+ *  Read light data.
+ *
+ *  @return true  Success
+ *  @return false Error
+ */
+bool px_ltr329als_meas_rd(px_ltr329als_meas_t * meas);
+
+/**
+ *  Convert measurement to lux
+ *
+ *  @param gain
+ *  @param int_time
+ *  @param meas
+ *
+ *  @return uint16_t
+ */
+uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t         gain,
+                                          px_ltr329als_int_time_t     int_time,
+                                          const px_ltr329als_meas_t * meas);
 
 /* _____MACROS_______________________________________________________________ */
 
--- devices/sensor/src/px_ltr329als.c
+++ devices/sensor/src/px_ltr329als.c
@@ -5,14 +5,14 @@
     |  __/   | |  | |___  | |_| | | |\  | | |_| | | |  | |  | |   /  \
     |_|     |___|  \____|  \___/  |_| \_|  \___/  |_|  |_| |___| /_/\_\
 
-    Copyright (c) 2019 Pieter Conradie <https://piconomix.com>
+    Copyright (c) 2025 Pieter Conradie <https://piconomix.com>
 
     License: MIT
     https://github.com/piconomix/px-fwlib/blob/master/LICENSE.md
 
-    Title:          Bosch LTR329ALS Barometric pressure sensor driver
+    Title:          px_ltr329als.h : Liteon LTR-329ALS ambient light sensor driver
     Author(s):      Pieter Conradie
-    Creation Date:  2019-09-12
+    Creation Date:  2025-07-07
 
 ============================================================================= */
 
@@ -21,67 +21,61 @@
 /* _____PROJECT INCLUDES_____________________________________________________ */
 #include "px_ltr329als.h"
 #include "px_i2c.h"
-#include "px_board.h"
+
 #include "px_log.h"
+PX_LOG_NAME("px_ltr329als")
 
 /* _____LOCAL DEFINITIONS____________________________________________________ */
-PX_LOG_NAME("ltr329als");
-
-/// @name Register Set (p. 12)
+/// @name registers
 /// @{
-#define PX_LTR329ALS_REG_ALS_CONTR                 0x80
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE             0x85
-#define PX_LTR329ALS_REG_PART_ID                   0x86
-#define PX_LTR329ALS_REG_MANUFAC_ID                0x87
-#define PX_LTR329ALS_REG_ALS_DATA_CH1_0            0x88
-#define PX_LTR329ALS_REG_ALS_DATA_CH1_1            0x89
-#define PX_LTR329ALS_REG_ALS_DATA_CH0_0            0x8a
-#define PX_LTR329ALS_REG_ALS_DATA_CH0_1            0x8b
-#define PX_LTR329ALS_REG_ALS_STATUS                0x8c
-
+#define PX_LTR329ALS_REG_ALS_CONTR           0x80
+#define PX_LTR329ALS_REG_ALS_MEAS_RATE       0x85
+#define PX_LTR329ALS_REG_PART_ID             0x86
+#define PX_LTR329ALS_REG_MANUFAC_ID          0x87
+#define PX_LTR329ALS_REG_ALS_DATA_CH1_0      0x88
+#define PX_LTR329ALS_REG_ALS_DATA_CH1_1      0x89
+#define PX_LTR329ALS_REG_ALS_DATA_CH0_0      0x8a
+#define PX_LTR329ALS_REG_ALS_DATA_CH0_1      0x8b
+#define PX_LTR329ALS_REG_STATUS              0x8c
 /// @}
 
-// ALS_CONTR Register (0x80)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_1X     (0 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_2X     (1 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_4X     (2 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_8X     (3 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_48X    (6 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_96X    (7 << 2)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_POS    2
-#define PX_LTR329ALS_REG_ALS_CONTR_SW_RESET        (1 << 1)
-#define PX_LTR329ALS_REG_ALS_CONTR_ALS_MODE        (1 << 0)
+/// Part Number ID value
+#define PX_LTR329ALS_PART_ID_VAL             0xa0
+/// Manufacturer ID value
+#define PX_LTR329ALS_MANUFAC_ID_VAL          0x05
 
-// ALS_MEAS_RATE Register (0x81)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_100MS   (0 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_50MS    (1 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_200MS   (2 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_400MS   (3 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_150MS   (4 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_250MS   (5 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_300MS   (6 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_350MS   (7 << 3)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_POS     3
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_50MS        (0 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_100MS       (1 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_200MS       (2 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_500MS       (3 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_1000MS      (4 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_2000MS      (5 << 0)
-#define PX_LTR329ALS_REG_ALS_MEAS_RATE_POS         0
+typedef union
+{
+    uint8_t u8;
 
-// PART_ID Register (0x86)
-#define PX_LTR329ALS_REG_PART_ID_PART_NUMBER_POS   4
-#define PX_LTR329ALS_REG_PART_ID_PART_NUMBER_MSK   (15 << 4)
-#define PX_LTR329ALS_REG_PART_ID_REV_POS           0
-#define PX_LTR329ALS_REG_PART_ID_REV_MSK           (15 << 0)
+    /// ALS_CONTR definition
+    struct
+    {
+        uint8_t als_mode : 1;       ///< Active mode: 0 = Stand-by mode (default); 1 = active mode
+        uint8_t sw_reset : 1;       ///< 0 = Initial start-up procedure is NOT started; 1 = Initial start-up procedure is started
+        uint8_t als_gain : 3;       ///< Gain selection: 0 = 1X; 1 = 2X; 2 = 4X; 3 = 8X; 6 = 48X; 7 = 96X
+        uint8_t reserved : 3;       ///< Set to 0
+    } als_contr;
 
-// ALS_STATUS Register (0x8c)
-#define PX_LTR329ALS_REG_ALS_STATUS_DATA_VALID     (1 << 7)
-#define PX_LTR329ALS_REG_ALS_STATUS_GAIN_POS       4
-#define PX_LTR329ALS_REG_ALS_STATUS_GAIN_MSK       (7 << 4)
-#define PX_LTR329ALS_REG_ALS_STATUS_INT            (1 << 3)
-#define PX_LTR329ALS_REG_ALS_STATUS_DATA           (1 << 2)
+    /// ALS_MEAS_RATE definition
+    struct
+    {
+        uint8_t als_meas_rate : 3;  ///< 0 = 50 ms; 1 = 100 ms; 2 = 200 ms; 3 = 500 ms (default); 4 = 1000 ms; 5,6,7 = 2000 ms
+        uint8_t als_int_time  : 3;  ///< 0 = 100 ms (default); 1 = 50 ms; 2 = 200 ms; 3 = 400 ms; 4 = 150 ms; 5 = 250 ms; 6 = 300 ms; 7 = 350 ms
+        uint8_t reserved      : 2;  ///< Set to 0
+    } als_meas_rate;
+
+    /// ALS_STATUS definition
+    struct
+    {
+        uint8_t reserved1           : 2;    ///< Don't care
+        uint8_t als_data_status     : 1;    ///< 0 = OLD data; 1 = NEW data
+        uint8_t reserved2           : 1;    ///< Don't care
+        uint8_t als_data_gain_range : 3;    ///< 0 = 1X (default); 1 = 2X; 2 = 4X; 3 = 8X; 4 = Invalid; 5 = Invalid; 6 = 48X; 7 = 96X
+        uint8_t als_data_valid      : 1;    ///< 0 = ALS Data is Valid (default); 1 = ALS Data is Invalid
+    } als_status;
+
+} px_ltr329als_reg_t;
 
 /* _____MACROS_______________________________________________________________ */
 
@@ -93,166 +87,190 @@ static px_i2c_handle_t * px_ltr329als_i2c_handle;
 /* _____LOCAL FUNCTION DECLARATIONS__________________________________________ */
 
 /* _____LOCAL FUNCTIONS______________________________________________________ */
-static bool px_ltr329als_reg_wr(uint8_t adr, uint8_t data)
+static bool px_ltr329als_wr(uint8_t reg, uint8_t val)
 {
-    uint8_t i2c_data[2];
+    uint8_t buf[2];
 
-    i2c_data[0] = adr;
-    i2c_data[1] = data;
+    buf[0] = reg;
+    buf[1] = val;
 
-    if(!px_i2c_wr(px_ltr329als_i2c_handle, i2c_data, 2, PX_I2C_FLAG_START_AND_STOP))
+    if(!px_i2c_wr(px_ltr329als_i2c_handle, buf, 2, PX_I2C_FLAG_START_AND_STOP))
     {
-        // Error
-        PX_LOG_E("Unable to write register value");
+        PX_LOG_E("Wr");
         return false;
     }
-    // Success
+
     return true;
 }
 
-static bool px_ltr329als_reg_rd(uint8_t adr, uint8_t * data)
+static bool px_ltr329als_rd(uint8_t reg, uint8_t * val)
 {
-    uint8_t i2c_data[1];
+    uint8_t buf[1];
 
-    i2c_data[0] = adr;
-
-    // Write address
-    if(!px_i2c_wr(px_ltr329als_i2c_handle, i2c_data, 1, PX_I2C_FLAG_START_AND_END))
+    buf[0] = reg;
+    if(!px_i2c_wr(px_ltr329als_i2c_handle, buf, 1, PX_I2C_FLAG_START_AND_END))
     {
-        // Error
-        PX_LOG_E("Unable to write register address");
+        PX_LOG_E("Wr");
         return false;
     }
-    // Read data
-    if(!px_i2c_rd(px_ltr329als_i2c_handle, data, 1, PX_I2C_FLAG_REP_START_AND_STOP))
+    if(!px_i2c_rd(px_ltr329als_i2c_handle, val, 1, PX_I2C_FLAG_REP_START_AND_STOP))
     {
-        // Error
-        PX_LOG_E("Unable to read register value");
+        PX_LOG_E("Rd");
         return false;
     }
-    // Success
+
     return true;
 }
 
-static bool px_ltr329als_reg_rd_data(uint8_t adr, void * data, uint8_t nr_of_bytes)
+static bool px_ltr329als_rd_data(uint8_t reg, void * data, uint8_t nr_of_bytes)
 {
-    uint8_t i2c_data[1];
+    uint8_t buf[1];
 
-    i2c_data[0] = adr;
-
-    // Write address
-    if(!px_i2c_wr(px_ltr329als_i2c_handle, i2c_data, 1, PX_I2C_FLAG_START_AND_END))
+    buf[0] = reg;
+    if(!px_i2c_wr(px_ltr329als_i2c_handle, buf, 1, PX_I2C_FLAG_START_AND_END))
     {
-        // Error
-        PX_LOG_E("Unable to write register address");
+        PX_LOG_E("Wr");
         return false;
     }
-    // Read data
     if(!px_i2c_rd(px_ltr329als_i2c_handle, data, nr_of_bytes, PX_I2C_FLAG_REP_START_AND_STOP))
     {
-        // Error
-        PX_LOG_E("Unable to read register data");
+        PX_LOG_E("Rd");
         return false;
     }
-    // Success
+
     return true;
 }
 
 /* _____GLOBAL FUNCTIONS_____________________________________________________ */
-bool px_ltr329als_init(px_i2c_handle_t * handle)
+bool px_ltr329als_init(px_i2c_handle_t *       handle,
+                       px_ltr329als_gain_t     gain,
+                       px_ltr329als_int_time_t int_time,
+                       px_ltr329als_meas_rate_t meas_rate)
 {
+    px_ltr329als_reg_t val;
+
+    px_board_delay_ms(100);
+
     // Save I2C slave handle
     px_ltr329als_i2c_handle = handle;
-    // Reset sensor
-    if(!px_ltr329als_reg_wr(PX_LTR329ALS_REG_ALS_CONTR, PX_LTR329ALS_REG_ALS_CONTR_SW_RESET))
+
+    // Verify Part ID
+    if(!px_ltr329als_rd(PX_LTR329ALS_REG_PART_ID, &val.u8))
     {
+        // Error
+        PX_LOG_E("Unable to read Part ID");
         return false;
     }
+    if(val.u8 != PX_LTR329ALS_PART_ID_VAL)
+    {
+        // Error
+        PX_LOG_E("Part ID fail (%02X)", val.u8);
+        return false;
+    }
+    // Verify Manufacturer ID
+    if(!px_ltr329als_rd(PX_LTR329ALS_REG_MANUFAC_ID, &val.u8))
+    {
+        // Error
+        PX_LOG_E("Unable to read Man ID");
+        return false;
+    }
+    if(val.u8 != PX_LTR329ALS_MANUFAC_ID_VAL)
+    {
+        // Error
+        PX_LOG_E("Man ID fail (%02X)", val.u8);
+        return false;
+    }
+    PX_LOG_I("LTR-329ALS found");
+    // Configure light sensor
+    val.u8                          = 0;
+    val.als_meas_rate.als_int_time  = int_time;
+    val.als_meas_rate.als_meas_rate = meas_rate;
+    if(!px_ltr329als_wr(PX_LTR329ALS_REG_ALS_MEAS_RATE, val.u8))
+    {
+        // Error
+        PX_LOG_E("Unable to set ALS_MEAS_RATE");
+        return false;
+    }
+    // Enable light sensor
+    val.u8                 = 0;
+    val.als_contr.als_mode = 1;
+    val.als_contr.als_gain = gain;
+    if(!px_ltr329als_wr(PX_LTR329ALS_REG_ALS_CONTR, val.u8))
+    {
+        // Error
+        PX_LOG_E("Unable to enable");
+        return false;
+    }
+
+    px_board_delay_ms(10);
+
     // Success
     return true;
 }
 
-bool px_ltr329als_rd_id(uint8_t * id)
+bool px_ltr329als_standby_mode(void)
 {
-    // Read manufacturer ID
-    if(!px_ltr329als_reg_rd(PX_LTR329ALS_REG_MANUFAC_ID, id))
+    return px_ltr329als_wr(PX_LTR329ALS_REG_ALS_CONTR, 0);
+}
+
+bool px_ltr329als_active_mode(px_ltr329als_gain_t gain)
+{
+    px_ltr329als_reg_t val;
+
+    val.u8                 = 0;
+    val.als_contr.als_mode = 1;
+    val.als_contr.als_gain = gain;
+    if(!px_ltr329als_wr(PX_LTR329ALS_REG_ALS_CONTR, val.u8))
     {
+        // Error
+        PX_LOG_E("Unable to enable");
         return false;
     }
-    else
-    {
-        return true;
-    }
-}
-
-void px_ltr329als_cfg(px_ltr329als_int_time_t int_time, px_ltr329als_meas_rate_t meas_rate)
-{
-    // Set integration time to 50 ms and measurement rate to 2 s
-    px_ltr329als_reg_wr(PX_LTR329ALS_REG_ALS_MEAS_RATE,
-                           (int_time  << PX_LTR329ALS_REG_ALS_MEAS_RATE_INT_POS)
-                         | (meas_rate << PX_LTR329ALS_REG_ALS_MEAS_RATE_POS));
-}
-
-void px_ltr329als_standby_mode(void)
-{
-    // Set sensor in standby mode
-    px_ltr329als_reg_wr(PX_LTR329ALS_REG_ALS_CONTR, 0);
-}
-
-void px_ltr329als_active_mode(px_ltr329als_gain_t gain)
-{
-    // Set sensor in active mode
-    px_ltr329als_reg_wr(PX_LTR329ALS_REG_ALS_CONTR,
-                          (gain << PX_LTR329ALS_REG_ALS_CONTR_ALS_GAIN_POS)
-                        | PX_LTR329ALS_REG_ALS_CONTR_ALS_MODE);
+    return true;
 }
 
 bool px_ltr329als_meas_ready(void)
 {
-    uint8_t data;
+    px_ltr329als_reg_t val;
 
-    // Read status register
-    if(!px_ltr329als_reg_rd(PX_LTR329ALS_REG_ALS_STATUS, &data))
+    if(!px_ltr329als_rd(PX_LTR329ALS_REG_STATUS, &val.u8))
     {
+        PX_LOG_E("Unable to rd status");
         return false;
     }
-    // New data available?
-    if(data & PX_LTR329ALS_REG_ALS_STATUS_DATA)
-    {
-        // Yes
-        return true;
-    }
-    else
-    {
-        // No
-        return false;
-    }
+
+    return (val.als_status.als_data_status != 0);
 }
 
-void px_ltr329als_meas_rd(uint16_t * ch0, uint16_t * ch1)
+bool px_ltr329als_meas_rd(px_ltr329als_meas_t * meas)
 {
-    uint8_t data[4];
-
     // Read Channel 1 and Channel 0 data
-    if(!px_ltr329als_reg_rd_data(PX_LTR329ALS_REG_ALS_DATA_CH1_0, data, 4))
+    if(!px_ltr329als_rd_data(PX_LTR329ALS_REG_ALS_DATA_CH1_0, (uint8_t *)meas, sizeof(*meas)))
     {
-        return;
+        PX_LOG_E("Unable to rd meas");
+        return false;
     }
-    // Assemble data
-    *ch1 = PX_U16_CONCAT_U8(data[1], data[0]);
-    *ch0 = PX_U16_CONCAT_U8(data[3], data[2]);
+    return true;
 }
 
-uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t     gain,
-                                          px_ltr329als_int_time_t int_time,
-                                          uint16_t                ch0,
-                                          uint16_t                ch1)
+uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t         gain,
+                                          px_ltr329als_int_time_t     int_time,
+                                          const px_ltr329als_meas_t * meas)
 {
     uint32_t ratio;
     uint32_t calc;
-    uint16_t lux;
+    uint32_t lux;
     uint8_t  gain_factor;
-    uint16_t int_factor;
+    uint8_t  int_factor;
+
+    uint32_t ch0 = meas->ch0;
+    uint32_t ch1 = meas->ch1;
+
+    if((ch0 == 0) && (ch1 == 0))
+    {
+        PX_LOG_E("Meas invalid");
+        return 0;
+    }
 
     // Get gain factor
     switch(gain)
@@ -285,22 +303,22 @@ uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t     gain,
     }
 
     // Use fast integer math instead of slow floating point
-    ratio = ((uint32_t)ch1 * 1024) / ((uint32_t)ch0 + (uint32_t)ch1);
+    ratio = (ch1 * 1024) / (ch0 + ch1);
 
-    if (ratio < (0.45 * 1024))
+    if (ratio < (uint32_t)(0.45 * 1024))
     {
-        calc =   (1.7743 * 1024) * (uint32_t)ch0
-               + (1.1059 * 1024) * (uint32_t)ch1;
+        calc =   (uint32_t)(1.7743 * 1024) * ch0
+               + (uint32_t)(1.1059 * 1024) * ch1;
     }
-    else if (ratio < (0.64 * 1024))
+    else if (ratio < (uint32_t)(0.64 * 1024))
     {
-        calc =   (4.2785 * 1024) * (uint32_t)ch0
-               - (1.9548 * 1024) * (uint32_t)ch1;
+        calc =   (uint32_t)(4.2785 * 1024) * ch0
+               - (uint32_t)(1.9548 * 1024) * ch1;
     }
-    else if (ratio < (0.85 * 1024))
+    else if (ratio < (uint32_t)(0.85 * 1024))
     {
-        calc =   (0.5926 * 1024) * (uint32_t)ch0
-               + (0.1185 * 1024) * (uint32_t)ch1;
+        calc =   (uint32_t)(0.5926 * 1024) * ch0
+               + (uint32_t)(0.1185 * 1024) * ch1;
     }
     else
     {
@@ -319,6 +337,5 @@ uint16_t px_ltr329als_meas_convert_to_lux(px_ltr329als_gain_t     gain,
         lux++;
     }
 
-    return lux;
+    return (uint16_t)lux;
 }
-
